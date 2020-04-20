d650e7cb8aaf5a8a6a9bf0f6e81e7df112440a01
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2020-04-21 00:59:33 +0200

    it works!

--- draft.js
+++ draft.js
@@ -1,4 +1,4 @@
-const segments = ["A_1_Va","A_1_Vl","A_2_Vl","A_3_Vl","Bass_1_Kb","Bass_1_Vc","Bass_2_Kb","Bass_2_Vc","Bass_3_Kb","Bass_3_Vc","B_1_Va","B_1_Vl","B_2_Va","B_2_Vl","B_3_Vl","C_1a_Vl","C_1b_Vl","C_2a_Vl","C_2b_Vl","C_3_Vl","C_4_Va","C_4_Vl","C_5_Va","C_6a_Vl","C_6b_Vl","D_1_Vl","E_1_Va","E_2_Vl","E_3_Vl","F_1_Vl","F_2_Vl","F_3_Va","F_4_Va"];
+const segment_names = ["A_1_Va","A_1_Vl","A_2_Vl","A_3_Vl","Bass_1_Kb","Bass_1_Vc","Bass_2_Kb","Bass_2_Vc","Bass_3_Kb","Bass_3_Vc","B_1_Va","B_1_Vl","B_2_Va","B_2_Vl","B_3_Vl","C_1a_Vl","C_1b_Vl","C_2a_Vl","C_2b_Vl","C_3_Vl","C_4_Va","C_4_Vl","C_5_Va","C_6a_Vl","C_6b_Vl","D_1_Vl","E_1_Va","E_2_Vl","E_3_Vl","F_1_Vl","F_2_Vl","F_3_Va","F_4_Va"];
 const extension = ".flac";
 const measure_length = 4/3;
 const measures = [
@@ -8,30 +8,30 @@ const measures = [
 	["A_1_Vl"],
 	["A_1_Vl"],
 	//5
-	["Bass_1_Vc","Bass_1_Kb","A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
+	["Bass_1_Vc","Bass_1_Kb","A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
 	//9a
-	["Bass_2_Vc","Bass_2_Kb","A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
+	["Bass_2_Vc","Bass_2_Kb","A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
 	//9b
-	["Bass_2_Vc","Bass_2_Kb","A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
+	["Bass_2_Vc","Bass_2_Kb","A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
+	["A_1_Vl","B_1_Va"],
 	//13
-	["Bass_2_Vc","Bass_2_Kb","A_1_Vl","C_1a_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
+	["Bass_2_Vc","Bass_2_Kb","C_4_Va","C_1a_Vl"],
+	["C_4_Va"],
+	["C_4_Va"],
+	["C_4_Va"],
 	//17
-	["Bass_2_Vc","Bass_2_Kb","A_1_Vl","C_1b_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
-	["A_1_Vl"],
+	["Bass_2_Vc","Bass_2_Kb","C_4_Va","C_1b_Vl"],
+	["C_4_Va"],
+	["C_4_Va"],
+	["C_4_Va"],
 	//21a
 	["Bass_2_Vc","Bass_2_Kb","A_1_Vl","B_1_Vl","C_4_Va"],
 	["B_1_Vl","C_4_Va"],
@@ -148,7 +148,7 @@ const measures = [
 	["F_1_Vl"],
 	[],
 	//92
-	["Bsss_3_Kb"],
+	["Bass_3_Kb"],
 	[],
 	["F_2_Vl"],
 	[],
@@ -164,3 +164,77 @@ const measures = [
 	[],
 	[]
 ]
+const segments = {};
+let ctx = new AudioContext();
+ctx.suspend();
+
+function loadAudio(name){
+	return new Promise((resolve, reject)=>{
+		fetch(name + extension, {method:"GET"})
+			.then(response => response.arrayBuffer())
+			.then(encodedAudio => ctx.decodeAudioData(encodedAudio))
+			.then(resolve)
+			.catch(reject)
+	});
+}
+function loadAllAudio(){
+	let totalAudioCount = segment_names.length;
+	let loadedAudioCount = 0;
+	const progress = document.getElementsByTagName("progress")[0];
+	progress.value = 0;
+	progress.max = totalAudioCount;
+	let promises = segment_names.map(name=>{
+		return loadAudio(name)
+			.then(audio => {
+				segments[name]=audio;
+				loadedAudioCount++;
+				progress.value = loadedAudioCount;
+				console.log("loaded %s/%s: %s", loadedAudioCount, totalAudioCount, name);
+				return audio;
+			}).catch(console.error)
+	});
+	return Promise.allSettled(promises);
+}
+function playAudio(buffer, time){
+	let source = ctx.createBufferSource();
+	source.buffer = buffer;
+	source.connect(ctx.destination);
+	source.start(time);
+}
+function getMeasure(index){
+	if(measures[index])	return measures[index].map(name => segments[name]);
+	else return [];
+}
+let start_time = 0;
+function playMeasure(index, time){
+	console.log("playing measure %s at time %s", index, time);
+	getMeasure(index).forEach(buffer=>playAudio(buffer, time));
+}
+let interval = 0.5;
+let plan_ahead = 1;
+let startindex = 0;
+function playloop(){
+	let ct = ctx.currentTime;
+	let stopindex = Math.ceil((ct + plan_ahead)/measure_length);
+	console.log({ct, startindex, stopindex});
+	for(let index = startindex; index <= stopindex; index++){
+		playMeasure(index, start_time + index * measure_length);
+	}
+	startindex = stopindex + 1;
+	setTimeout(playloop, interval*1000);
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	loadAllAudio()
+		.then(()=>{
+			let playbutton = document.getElementById("play");
+			playbutton.removeAttribute("disabled");
+			playbutton.addEventListener("click", ()=>{
+				startindex = 0;
+				start_time = ctx.currentTime;
+				ctx.resume();
+			});
+			playloop();
+		});
+});
