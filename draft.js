02e7af0dc0684e82fecc826d197500b55116fe08
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2020-04-20 23:25:00 +0200

    first commit

---
+++ draft.js
@@ -0,0 +1,17 @@
+const segments = ["A_1_Va","A_1_Vl","A_2_Vl","A_3_Vl","Bass_1_Kb","Bass_1_Vc","Bass_2_Kb","Bass_2_Vc","Bass_3_Kb","Bass_3_Vc","B_1_Va","B_1_Vl","B_2_Va","B_2_Vl","B_3_Vl","C_1a_Vl","C_1b_Vl","C_2a_Vl","C_2b_Vl","C_3_Vl","C_4_Va","C_4_Vl","C_5_Va","D_1_Vl","E_1_Va","E_2_Vl","E_3_Vl","F_1_Vl","F_2_Vl","F_3_Va","F_4_Va"];
+const extension = ".flac";
+const measures = [
+	//1
+	["Bass_2_Vc"],
+	[],
+	["A_1_Vl"],
+	["A_1_Vl"],
+	//5
+	["Bass_1_Vc","Bass_1_Kb","A_1_Vl"],
+	["A_1_Vl"],
+	["A_1_Vl"],
+	["A_1_Vl"],
+	//9a
+	["Bass_2_Vc","Bass_2_Kb","A_1_Vl"],
+	["A_1_Vl"],
+]
